0c0b6e99900c09f499c36a8b53427ef868d2ee7f
grizzzli74 <ivanusolcev74@gmail.com>
2024-01-12 20:50:59 +0300

    Fix matrix
    1. Не считает сумму по столбцам. Только по строкам.
    2. При конфигурации выводит только одну строку, хотя возможные значения для заполнения матрицы еще есть.

--- matrix.php
+++ matrix.php
@@ -3,7 +3,7 @@
 
 /**
  * Функция принимает три аргумента: размер матрицы ($n х $m), максимальное число для генерации ($q) и массив использованных чисел ($usedNumbers).
- * Далее она создает двумерный массив (матрицу) и в цикле заполняет каждую строку случайными числами. Перед добавлением числа в массив проверяется, не было ли оно уже использовано. Если было, то генерируется новое случайное число. Если уникальные числа закончились, то выходит из итерации.
+ * Далее она создает двумерный массив (матрицу) и в цикле заполняет каждую строку случайными числами. Перед добавлением числа в массив проверяется, не было ли оно уже использовано. Если было, то генерируется новое случайное число. Если уникальные числа закончились, то дописывает строку до конца с нулевыми значениями номера и выходит из итерации.
  *
  * @param int $n
  * @param int $m
@@ -12,33 +12,65 @@
  */
 function generateMatrix(int $n, int $m, int $q): void
 {
+	$matrix = [];
 	$usedNumbers = [];
+	$matrixColumnSum = 0;
 
 	for ($i = 0; $i < $m; $i++) {
-		$tempRow = [];
-
+		$tempRow = [
+			'row' => [],
+			'sum' => 0,
+		];
 		for ($j = 0; $j < $n; $j++) {
-			if (count($usedNumbers) >= $q) return; // можно было бы использовать goto end или break
+			$number = 0;
+			if (count($usedNumbers) < $q) {
+				do {
+					//Генерация случайного числа
+					$number = rand(1, $q);
 
-			$number = rand(1, $q);
+					//Проверка на уникальность числа
+				} while (in_array($number, $usedNumbers));
+			};
 
-			while (in_array($number, $usedNumbers)) {
-				$number = rand(1, $q);
-			}
-
-			$tempRow[] = $number;
+			$tempRow['row'][] = $number;
+			$tempRow['sum'] += $number;
 			$usedNumbers[] = $number;
 		}
 
-		print_r(implode(' ', $tempRow));
-		print_r('  ');
-		print_r(array_sum($tempRow));
-		print(PHP_EOL);
+		$matrix[] = $tempRow;
+
+		if (count($usedNumbers) >= $q) break;
 	}
+
+	foreach ($matrix as $row) {
+		if (!$row['sum']) {
+			continue;
+		}
+
+		$matrixColumnSum += $row['sum'];
+
+		foreach ($row['row'] as $number) {
+			$format = "% " . strlen($q);
+			if ($number === 0) {
+				$format .= "s ";
+				print_r(sprintf($format, ''));
+				continue;
+			}
+
+			$format .= "d ";
+			print_r(sprintf($format, $number));
+		}
+
+		print_r('  ');
+		print_r($row['sum']);
+		print_r(PHP_EOL);
+	}
+
+	print_r("Сумма столбцов: $matrixColumnSum" . PHP_EOL);
 }
 
 // Вызов функции
-$n = 3;
-$m = 2;
-$q = 9;
+$n = 10;
+$m = 10;
+$q = 78;
 generateMatrix($n, $m, $q);
